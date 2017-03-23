240bb087bb5b0a5d4cdc989013bc5e7a60e6026e
Hanna <ihamnya@gmail.com>
2017-03-23 15:19:01 +0900

    Initial commit

---
+++ app/src/main/java/com/hamnya/cardview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hamnya.cardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * 출처: http://jizard.tistory.com/23
+ * This is card view example using RecyclerView.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
