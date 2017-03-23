f8da7ea4262787879d2b04fb7f6e9635c457ccde
Hanna <ihamnya@gmail.com>
2017-03-23 17:43:41 +0900

    이상한 임폴트 제거

--- app/src/main/java/com/hamnya/cardview/MainActivity.java
+++ app/src/main/java/com/hamnya/cardview/MainActivity.java
@@ -1,6 +1,5 @@
 package com.hamnya.cardview;
 
-import android.support.v7.app.AlertController;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
